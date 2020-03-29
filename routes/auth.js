98f9cddc86756088351a09d466f924aaec006443
BobbaTea <bobba.akash@gmail.com>
2020-03-28 22:33:03 -0400

    bruosdbjkf

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,6 @@ router.post('/signup', (req, res) => {
     var password = req.body['password'];
     var name = req.body['name'];
     var userType = req.body['userType'];
-<<<<<<< HEAD
-    req.session.email = "vsduf"
-    if (email === "" || password === "" || name === "" || userType === "") {
-        res.send({
-            "status": "incomplete_fields"
-        });
-=======
 
     if (userType === 'seller') {
         if (email === "" || password === "" || name === "") {
@@ -26,7 +19,6 @@ router.post('/signup', (req, res) => {
                 "status": "incomplete_fields"
             });
         }
->>>>>>> 19fd6c1f483e1ed2e39395a679a52b0a437c7ce5
     }
 
     res.send({
