c2c434e2f8254afbfae4a288e4aef46f21a5a844
BobbaTea <bobba.akash@gmail.com>
2020-03-28 20:42:58 -0400

    yeet

--- app.js
+++ app.js
@@ -20,40 +20,40 @@ app.set('view engine', 'handlebars');
 // app.use(helmet())
 app.use(bodyParser.json());
 
-// app.use(cookieParser('betajithisisvarun'));
+app.use(cookieParser('betajithisisvarun'));
 
 app.use('/static', express.static('static'));
 
-// var store = new MongoDBStore(
-//     {
-//         uri: config.database.uri,
-//         databaseName: config.database.db,
-//         collection: 'sessions'
-//     },
-//     function (error) {
-//         // console.log(error)
-//     });
-// store.on('error', function (error) {
-//     // console.log(error)
-// });
+var store = new MongoDBStore(
+    {
+        uri: config.database.uri,
+        databaseName: config.database.db,
+        collection: 'sessions'
+    },
+    function (error) {
+        // console.log(error)
+    });
+store.on('error', function (error) {
+    // console.log(error)
+});
 
-// app.use(session({
-//     secret: config.session.secret,
-//     cookie: {
-//         maxAge: config.session.cookie.maxAge,
-//         sameSite: config.session.cookie.sameSite
-//     },
-//     store: store,
-//     resave: config.session.resave,
-//     rolling: config.session.rolling,
-//     saveUninitialized: config.session.saveUninitialized
-// }));
-// app.use(flash());
-// app.use((req, res, next) => {
-//     //Use this middleware section for handling user auth and session stuff
+app.use(session({
+    secret: config.session.secret,
+    cookie: {
+        maxAge: config.session.cookie.maxAge,
+        sameSite: config.session.cookie.sameSite
+    },
+    store: store,
+    resave: config.session.resave,
+    rolling: config.session.rolling,
+    saveUninitialized: config.session.saveUninitialized
+}));
+app.use(flash());
+app.use((req, res, next) => {
+    //Use this middleware section for handling user auth and session stuff
 
-//     next()
-// });
+    next()
+});
 
 app.use('/', require('./routes/index'));
 app.use('/api/auth', require('./routes/authentication'));
